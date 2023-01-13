08c69d7127457e1c65429685ea19c6b349061636
jcothran@edjsports.com <johnwcothran@gmail.com>
2023-01-13 03:56:55 -0500

    attempt at jsdom fix. not working

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ const customJestConfig = {
  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
  moduleDirectories: ['node_modules', '<rootDir>/'],
- testEnvironment: 'jsdom',
+ testEnvironment: 'jest-environment-jsdom',
  setupFilesAfterEnv: ['isomorphic-unfetch'],
 }
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
